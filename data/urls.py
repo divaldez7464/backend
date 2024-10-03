e7f95b16b8f8bad0928c36f6d8333bb7cff7ef3b
Diego Valdez <divaldez@csumb.edu>
2024-10-02 22:27:06 -0700

    Added the google oauh for api

--- data/urls.py
+++ data/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    
+    path('admin/', admin.site.urls),
+    path('auth/', include('social_django.urls', namespace='social')),  # Google OAuth
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('data.urls')),
 ]
