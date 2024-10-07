8a591346d6497f19fbcdb86eb583d1e655ff43d4
divaldez7464 <divaldez@csumb.edu>
2024-10-07 15:02:30 -0700

    Update with latest changes

--- manage.py
+++ manage.py
@@ -15,8 +15,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    execute_from_command_line("web: gunicorn project02.wsgi --log-file -")
 
 
 if __name__ == "__main__":
     main()
+
