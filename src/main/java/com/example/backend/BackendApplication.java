f82d9ce86e6bce316e23e026a90675c691f275f6
divaldez7464 <divaldez@csumb.edu>
2024-10-07 19:11:09 -0700

    new backend

---
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -0,0 +1,24 @@
+package com.example.backend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@CrossOrigin
+	@GetMapping("/hello")
+	public String hello(@RequestParam (value="name", defaultValue="springboot")String name) {
+		return String.format("Hello %s", name);
+	}
+
+
+}
