31c7636350d4b3f787b71dba2d7f3adc299155ba
Enrique <enrangel@csumb.edu>
2024-10-20 18:16:06 -0700

    heroku corsfilter

---
+++ src/main/java/com/example/backend/controllers/AdminController.java
@@ -0,0 +1,16 @@
+// package com.example.backend.controllers;
+
+// import com.example.backend.entity.Admin;
+// import com.example.backend.entity.User;
+// import com.example.backend.repository.ItemRepository;
+// import com.example.backend.repository.UserRepository;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+
+// import java.math.BigDecimal;
+// import java.util.List;
+
+// public class AdminController {
+    
+// }
---
+++ src/main/java/com/example/backend/services/Adminservice.java
@@ -0,0 +1,5 @@
+package com.example.backend.services;
+
+public class Adminservice {
+    
+}
