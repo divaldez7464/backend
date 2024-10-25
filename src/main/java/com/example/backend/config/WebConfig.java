cbb5df37aaeca6f83f671a7213e8ad30b6f56a93
erikgovea <118408955+erikgovea@users.noreply.github.com>
2024-10-25 14:17:10 -0700

    Update WebConfig.java
    adding the asbilty for heroku to be allowed in orgins

--- src/main/java/com/example/backend/config/WebConfig.java
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:3000") // Update this to your React app's URL
+                .allowedOrigins("http://localhost:3000", "https://project02-3bd6df9baeaf.herokuapp.com") // Update this to your React app's URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow cookies or authentication
